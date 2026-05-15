a2591d89499cef76e5429d159cb59505cf14ac7f
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-05-15 12:01:12 +0530

    Time: 1 ms (96.67%), Space: 42.2 MB (96.44%) - LeetHub

---
+++ 0231-power-of-two/0231-power-of-two.java
@@ -0,0 +1,11 @@
+class Solution {
+    public boolean isPowerOfTwo(int n) {
+        if(n==1)return true;
+        if(n<=0)return false;
+        while(n >1){
+            if((n & 1)==1)return false;
+            n/=2;
+        }
+        return true;
+    }
+}
