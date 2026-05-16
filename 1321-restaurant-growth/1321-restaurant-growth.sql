c6020428ba94b66b9b21bc193dca050f1d38aa0b
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-05-17 01:26:42 +0530

    Time: 379 ms (30.43%), Space: 0B (100%) - LeetHub

---
+++ 1321-restaurant-growth/1321-restaurant-growth.sql
@@ -0,0 +1,16 @@
+SELECT 
+    a.visited_on,
+    SUM(b.amount) AS amount,
+    ROUND(SUM(b.amount) / 7, 2) AS average_amount
+FROM (
+    SELECT DISTINCT visited_on 
+    FROM Customer
+) a
+JOIN Customer b 
+    ON DATEDIFF(a.visited_on, b.visited_on) BETWEEN 0 AND 6
+WHERE a.visited_on >= (
+    SELECT DATE_ADD(MIN(visited_on), INTERVAL 6 DAY) 
+    FROM Customer
+)
+GROUP BY a.visited_on
+ORDER BY a.visited_on;
