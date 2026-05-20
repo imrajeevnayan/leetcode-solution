f5d0f62425823cdf902b2c64404f9761e71c3490
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-05-20 17:01:08 +0530

    Time: 2674 ms (5%), Space: 55.3 MB (98.78%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -2,18 +2,18 @@ class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
          Set<List<Integer>> result = new HashSet<>();
     for (int i = 0; i < nums.length; i++) {
-        Set<Integer> seen = new HashSet<>();
+        Set<Integer> set = new HashSet<>();
         for (int j = i + 1; j < nums.length; j++) {
             int third = -nums[i] - nums[j];
-            if (seen.contains(third)) {
+            if (set.contains(third)) {
                 List<Integer> triplet =
                     Arrays.asList(nums[i], nums[j], third);
                 Collections.sort(triplet);
                 result.add(triplet);
             }
-            seen.add(nums[j]);
+            set.add(nums[j]);
         }
      }
-       return new ArrayList<>(result);    
+      return new ArrayList<>(result); 
     }
 }
