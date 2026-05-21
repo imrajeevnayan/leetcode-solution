86074623b0fb7b03a0ca09f1e12922fd4c0fa137
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-05-21 23:25:48 +0530

    Time: 16 ms (37.36%), Space: 46.8 MB (37%) - LeetHub

--- 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
+++ 0451-sort-characters-by-frequency/0451-sort-characters-by-frequency.java
@@ -7,9 +7,7 @@ class Solution {
     StringBuilder sb=new StringBuilder();
     while(!pq.isEmpty()){
         char c=pq.poll();
-        for(int i=0;i<map.get(c);i++){
-            sb.append(c);
-           }
+        for(int i=0;i<map.get(c);i++) sb.append(c);    
      }
      return sb.toString();   
     }
