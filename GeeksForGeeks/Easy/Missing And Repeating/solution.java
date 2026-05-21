542459a2db2a21767806b9074b4811a0b6312d22
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-05-21 22:48:49 +0530

    [AlgoCommit] GeeksForGeeks/Easy/Missing And Repeating

---
+++ GeeksForGeeks/Easy/Missing And Repeating/solution.java
@@ -0,0 +1,16 @@
+class Solution {
+    ArrayList<Integer> findTwoElement(int arr[]) {
+        int n = arr.length;
+        int[] freq = new int[n + 1];
+        for (int num : arr) freq[num]++;
+        int repeating = -1, missing = -1;
+        for (int i = 1; i <= n; i++) {
+            if (freq[i] == 2) repeating = i;
+            if (freq[i] == 0) missing = i;
+        }
+        ArrayList<Integer> ans = new ArrayList<>();
+        ans.add(repeating);
+        ans.add(missing);
+        return ans;
+    }
+}
--- README.md
+++ README.md
@@ -1,3 +1,4 @@
 | # | Problem | Platform | Difficulty | Date Solved | Time |
 |---|---------|----------|------------|-------------|------|
 | 102 | [Top K Frequent in Array](https://www.geeksforgeeks.org/problems/top-k-frequent-elements-in-array/1?utm=codolio) | GeeksForGeeks | Medium | 21 May 2026 | 10:36 pm |
+| 2 | [Missing And Repeating](https://www.geeksforgeeks.org/problems/find-missing-and-repeating2512/1?utm_source=chatgpt.com) | GeeksForGeeks | Easy | 21 May 2026 | 10:48 pm |
