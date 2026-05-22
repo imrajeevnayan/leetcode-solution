a7ffd551e995a452020a47e4d9190eba3a2edeb5
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-05-22 10:38:38 +0530

    [AlgoCommit] GeeksForGeeks/Medium/Next Smaller Element

---
+++ GeeksForGeeks/Medium/Next Smaller Element/solution.java
@@ -0,0 +1,15 @@
+class Solution {
+    static ArrayList<Integer> nextSmallerEle(int[] arr) {
+        int n = arr.length;
+        ArrayList<Integer> result = new ArrayList<>(Collections.nCopies(n, -1));
+        Stack<Integer> st = new Stack<>();
+        for(int i=0;i<n;i++){
+            while(!st.isEmpty()&& arr[i]<arr[st.peek()]){
+              int index=st.pop();
+              result.set(index,arr[i]);
+           }
+           st.push(i);
+      }
+        return result;
+    }
+}
