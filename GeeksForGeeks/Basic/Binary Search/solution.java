268cbd04101f4efdbb1f6fe22fcb47ae1bc46f81
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-05-25 12:36:09 +0530

    [AlgoCommit] GeeksForGeeks/Basic/Binary Search

---
+++ GeeksForGeeks/Basic/Binary Search/solution.java
@@ -0,0 +1,8 @@
+class Solution {
+    public boolean binarySearch(int[] arr, int k) {
+        for(int i=0;i<arr.length;i++){
+            if(arr[i]==k)return true;
+        }
+        return false;
+    }
+}
