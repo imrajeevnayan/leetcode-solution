cc95e3303f387bdf2249618da11732eb0bbc2448
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-05-30 01:31:58 +0530

    Time: 0 ms (100%), Space: 42.2 MB (98.24%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -1,14 +1,28 @@
 class Solution {
     public int rob(int[] nums) {
-        if(nums.length==0)return 0;
-        if(nums.length==1)return nums[0];
-        int n=nums.length;
-        int dp[]=new int[n];
-        dp[0]=nums[0];
-        dp[1]=Math.max(nums[0],nums[1]);
-        for(int i=2;i<n;i++){
-            dp[i]=Math.max(nums[i]+dp[i-2],dp[i-1]);
+        int[] dp = new int[nums.length];
+
+        for (int i = 0; i < nums.length; i++) {
+            dp[i] = -1;
         }
-        return dp[n-1];
+
+        return solve(nums, 0, dp);
+    }
+
+    private int solve(int[] nums, int idx, int[] dp) {
+        if (idx >= nums.length) {
+            return 0;
+        }
+
+        if (dp[idx] != -1) {
+            return dp[idx];
+        }
+
+        int take = nums[idx] + solve(nums, idx + 2, dp);
+        int skip = solve(nums, idx + 1, dp);
+
+        dp[idx] = Math.max(take, skip);
+
+        return dp[idx];
     }
 }
