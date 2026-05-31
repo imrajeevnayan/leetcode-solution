8428dec9d1ab86e369e3c3b42a4b79ef9b37d5e6
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-05-31 12:28:41 +0530

    Time: 6 ms (38.96%), Space: 58.6 MB (5.16%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,12 +1,24 @@
 class Solution {
     public boolean isPalindrome(String s) {
-     String s1=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
-     int left=0,right=s1.length()-1;
-       while(left < right){
-        if(s1.charAt(left) !=s1.charAt(right) ) return  false;
-          left++;
-          right--;
-       }
-       return true; 
+        return check(s, 0, s.length() - 1);
+    }
+
+    private boolean check(String s, int start, int end) {
+        if (start >= end) return true;
+        char left = s.charAt(start);
+        char right = s.charAt(end);
+        if (!Character.isLetterOrDigit(left)) {
+            return check(s, start + 1, end);
+        }
+
+        if (!Character.isLetterOrDigit(right)) {
+            return check(s, start, end - 1);
+        }
+
+        if (Character.toLowerCase(left) != Character.toLowerCase(right)) {
+            return false;
+        }
+
+        return check(s, start + 1, end - 1);
     }
 }
