d181de05504ceb19582996f081686dc341ddf5e1
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-06-03 15:55:31 +0530

    Time: 0 ms (100%), Space: 43.1 MB (32.12%) - LeetHub

--- 0058-length-of-last-word/0058-length-of-last-word.java
+++ 0058-length-of-last-word/0058-length-of-last-word.java
@@ -3,8 +3,7 @@ class Solution {
         int count=0;
         for(int i=s.length()-1;i>=0;i--){
             if(s.charAt(i)!=' ')count++;
-            else if (count > 0)
-                 break;
+            else if (count > 0) break;
             }
         return count;
     }
