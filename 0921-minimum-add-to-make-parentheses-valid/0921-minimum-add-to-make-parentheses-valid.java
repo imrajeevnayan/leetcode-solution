10cc6bb090f5d582befbb1a2c369ef468fa23dc0
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-06-04 22:54:08 +0530

    Time: 0 ms (100%), Space: 42.7 MB (73.65%) - LeetHub

---
+++ 0921-minimum-add-to-make-parentheses-valid/0921-minimum-add-to-make-parentheses-valid.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int minAddToMakeValid(String s) {
+        int left=0,right=0;
+        for(char c:s.toCharArray()){
+            if(c=='(') left++;
+            else{
+                if(left >0) left--;
+                else right++;
+            }
+        }
+        return left+right;
+    }
+}
