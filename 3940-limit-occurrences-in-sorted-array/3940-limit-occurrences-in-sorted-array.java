05125acbaad8c9e8cb9f5a05b2f69e018b7aae43
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-06-05 23:10:24 +0530

    Time: 1 ms (100%), Space: 46.6 MB (92.17%) - LeetHub

---
+++ 3940-limit-occurrences-in-sorted-array/3940-limit-occurrences-in-sorted-array.java
@@ -0,0 +1,26 @@
+class Solution {
+    public int[] limitOccurrences(int[] nums, int k) {
+        List<Integer> result = new ArrayList<>();
+        int n = nums.length;
+        
+        for (int i = 0; i < n; ) {
+            int j = i;
+            while (j < n && nums[j] == nums[i]) {
+                j++;
+            }
+            
+            // Add min(k, count) times
+            int count = Math.min(k, j - i);
+            for (int t = 0; t < count; t++) {
+                result.add(nums[i]);
+            }
+            i = j;
+        }
+        
+        int[] ans = new int[result.size()];
+        for (int i = 0; i < ans.length; i++) {
+            ans[i] = result.get(i);
+        }
+        return ans;
+    }
+}
