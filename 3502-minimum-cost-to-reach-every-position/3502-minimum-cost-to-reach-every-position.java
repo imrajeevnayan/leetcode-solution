ebbf94b23e413e34ff2dcaeca978b088defe9857
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-06-06 14:18:15 +0530

    Time: 1 ms (91.25%), Space: 46.7 MB (70.72%) - LeetHub

---
+++ 3502-minimum-cost-to-reach-every-position/3502-minimum-cost-to-reach-every-position.java
@@ -0,0 +1,15 @@
+class Solution {
+    public int[] minCosts(int[] cost) {
+        int n = cost.length;
+        int[] ans = new int[n];
+        
+        if (n == 0) return ans;
+        ans[0] = cost[0];
+        
+        for (int i = 1; i < n; i++) {
+            ans[i] = Math.min(ans[i - 1], cost[i]);
+        }
+        
+        return ans;
+    }
+}
