694f1665229a901e3e553f8989e2a5629a8cb6ee
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-06-06 14:05:55 +0530

    Time: 3 ms (61.01%), Space: 52.8 MB (9.68%) - LeetHub

---
+++ 0485-max-consecutive-ones/0485-max-consecutive-ones.java
@@ -0,0 +1,12 @@
+class Solution {
+    public int findMaxConsecutiveOnes(int[] nums) {
+       int count=0,maxCount=0;
+       for(int n:nums){
+        if(n==1){count++;
+        maxCount=Math.max(maxCount,count);
+        }
+        else count=0;
+       }
+    return  maxCount;
+    }
+}
