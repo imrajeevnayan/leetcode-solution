30c2b5d2914598987c674dfc246cfdeb09eefb77
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-06-08 15:44:02 +0530

    Time: 0 ms (100%), Space: 43 MB (7.79%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -0,0 +1,25 @@
+class Solution {
+    public int rob(int[] nums) {
+        if (nums == null || nums.length == 0) return 0;
+        if (nums.length == 1) return nums[0];
+        
+        // Scenario 1: Exclude the last house
+        int option1 = robLinear(nums, 0, nums.length - 2);
+        
+        // Scenario 2: Exclude the first house
+        int option2 = robLinear(nums, 1, nums.length - 1);
+        
+        return Math.max(option1, option2);
+    }
+        private int robLinear(int[] nums, int start, int end) {
+        int prev1 = 0; // Max profit up to i-1
+        int prev2 = 0; // Max profit up to i-2
+        
+        for (int i = start; i <= end; i++) {
+            int current = Math.max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = current;
+        }
+        return prev1;
+    }
+}
