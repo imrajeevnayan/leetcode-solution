c21da6856bb59ea33aa894ec21963b5b0eefdb72
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-06-14 12:13:33 +0530

    Time: 0 ms (100%), Space: 44.6 MB (7.21%) - LeetHub

--- 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
+++ 0021-merge-two-sorted-lists/0021-merge-two-sorted-lists.java
@@ -4,11 +4,12 @@ class Solution {
         if (list2 == null) return list1;
         
         if (list1.val <= list2.val) {
-            list1.next = mergeTwoLists(list1.next, list2);return list1;
+            list1.next = mergeTwoLists(list1.next, list2);
+            return list1;
         } 
         else {
-            list2.next = mergeTwoLists(list1, list2.next); return list2;
-        }
-        
+            list2.next = mergeTwoLists(list1, list2.next);
+            return list2;
+        }   
     }
 }
