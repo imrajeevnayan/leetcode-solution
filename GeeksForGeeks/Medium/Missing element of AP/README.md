d61a22a5ee08a8ebb449d3514a1101dd2f5eb132
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-06-14 22:19:58 +0530

    [AlgoCommit] GeeksForGeeks/Medium/Missing element of AP

---
+++ GeeksForGeeks/Medium/Missing element of AP/README.md
@@ -0,0 +1,13 @@
+# Missing element of AP
+
+## Difficulty: Medium
+
+## Platform: GeeksForGeeks
+
+## Problem Link
+[View Problem](https://www.geeksforgeeks.org/problems/missing-element-of-ap2228/1)
+
+## Solved On
+14 Jun 2026 at 10:19 pm
+
+<h2><a href="https://www.geeksforgeeks.org/problems/missing-element-of-ap2228/1">Missing element of AP</a></h2><h3>Difficulty Level: Medium</h3><hr>
---
+++ GeeksForGeeks/Medium/Missing element of AP/solution.java
@@ -0,0 +1,30 @@
+class Solution {
+    public int findMissing(int[] arr) {
+         int n = arr.length;
+             if (n == 2) {
+            int d = arr[1] - arr[0];
+            return arr[1] + d;
+        }
+        int d1 = arr[1] - arr[0];
+        int d2 = arr[2] - arr[1];
+        int d3 = arr[n-1] - arr[n-2];
+        
+        int d;
+        if (d1 == d2 || d1 == d3) {
+            d = d1;
+        } else if (d2 == d3) {
+            d = d2;
+        } else {
+            d = d3;
+        }
+        
+        for (int i = 1; i < n; i++) {
+            if (arr[i] - arr[i-1] != d) {
+                return arr[i-1] + d;
+            }
+        }
+        
+        return arr[n-1] + d;
+        
+    }
+}
