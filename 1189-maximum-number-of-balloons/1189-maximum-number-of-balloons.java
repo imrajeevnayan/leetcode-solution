6808fa03428e320586ce8a869bd41df86821700b
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-06-22 12:28:16 +0530

    Time: 10 ms (43.98%), Space: 44.3 MB (8.64%) - LeetHub

---
+++ 1189-maximum-number-of-balloons/1189-maximum-number-of-balloons.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int maxNumberOfBalloons(String text) {
+        Map<Character, Integer> map = new HashMap<>();
+        for (char ch : text.toCharArray())map.put(ch, map.getOrDefault(ch, 0) + 1);
+
+        return Math.min(
+                Math.min(map.getOrDefault('b', 0),
+                         map.getOrDefault('a', 0)),
+                Math.min(
+                    Math.min(map.getOrDefault('l', 0) / 2,
+                             map.getOrDefault('o', 0) / 2),
+                    map.getOrDefault('n', 0)
+                )
+        );
+    }
+}
