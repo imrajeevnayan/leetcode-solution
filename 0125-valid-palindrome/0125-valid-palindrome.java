cfe006881ad622ffd6ffe1c2a733dee23e76887f
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-06-29 14:53:11 +0530

    Time: 14 ms (32.22%), Space: 46 MB (37.61%) - LeetHub

--- 0125-valid-palindrome/0125-valid-palindrome.java
+++ 0125-valid-palindrome/0125-valid-palindrome.java
@@ -1,24 +1,12 @@
 class Solution {
     public boolean isPalindrome(String s) {
-        return check(s, 0, s.length() - 1);
-    }
-
-    private boolean check(String s, int start, int end) {
-        if (start >= end) return true;
-        char left = s.charAt(start);
-        char right = s.charAt(end);
-        if (!Character.isLetterOrDigit(left)) {
-            return check(s, start + 1, end);
+        String s1=s.replaceAll("[^a-zA-Z0-9]","").toLowerCase();
+        int left=0,right=s1.length()-1;
+        while(left<right){
+            if(s1.charAt(left)!=s1.charAt(right))return false;
+            left++;
+            right--;
         }
-
-        if (!Character.isLetterOrDigit(right)) {
-            return check(s, start, end - 1);
-        }
-
-        if (Character.toLowerCase(left) != Character.toLowerCase(right)) {
-            return false;
-        }
-
-        return check(s, start + 1, end - 1);
+        return true;
     }
 }
