4ed14bc91541fb78a51d8a07f6bd119a229316d9
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-06-30 14:57:56 +0530

    Time: 163 ms (6.86%), Space: 47.9 MB (5.26%) - LeetHub

--- 0018-4sum/0018-4sum.java
+++ 0018-4sum/0018-4sum.java
@@ -1,30 +1,22 @@
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
-        List<List<Integer>> result = new ArrayList<>();
+        Set<List<Integer>> result = new HashSet<>();
         Arrays.sort(nums);
         int n = nums.length;
-
+        
         for (int i = 0; i < n - 3; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-
             for (int j = i + 1; j < n - 2; j++) {
-                if (j > i + 1 && nums[j] == nums[j - 1]) continue;
-
-                int left = j + 1;
-                int right = n - 1;
-
+                long remaining = (long) target - nums[i] - nums[j];
+                int left = j + 1, right = n - 1;
+                
                 while (left < right) {
-                    long sum = (long) nums[i] + nums[j] + nums[left] + nums[right];
-
-                    if (sum == target) {
+                    long sum = (long) nums[left] + nums[right];
+                    
+                    if (sum == remaining) {
                         result.add(Arrays.asList(nums[i], nums[j], nums[left], nums[right]));
-
-                        while (left < right && nums[left] == nums[left + 1]) left++;
-                        while (left < right && nums[right] == nums[right - 1]) right--;
-
                         left++;
                         right--;
-                    } else if (sum < target) {
+                    } else if (sum < remaining) {
                         left++;
                     } else {
                         right--;
@@ -32,7 +24,7 @@ class Solution {
                 }
             }
         }
-
-        return result;
+        
+        return new ArrayList<>(result);
     }
 }
