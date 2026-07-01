479bc3a87109fab95942a53315d1562f4e4b44eb
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-07-01 12:19:48 +0530

    Time: 0 ms (100%), Space: 42.7 MB (74.18%) - LeetHub

--- 0198-house-robber/0198-house-robber.java
+++ 0198-house-robber/0198-house-robber.java
@@ -5,14 +5,9 @@ class Solution {
         return solve(nums, 0, dp);
     }
     private int solve(int[] nums, int idx, int[] dp) {
-        if (idx >= nums.length) {
-            return 0;
-        }
-
-        if (dp[idx] != -1) {
-            return dp[idx];
-        }
-
+        if (idx >= nums.length)  return 0;
+        if (dp[idx] != -1)  return dp[idx];
+    
         int take = nums[idx] + solve(nums, idx + 2, dp);
         int skip = solve(nums, idx + 1, dp);
 
