bb1040d9a584efe153ffc4a6699556a2f66b2a95
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-07-02 22:23:31 +0530

    Time: 0 ms (100%), Space: 44.1 MB (99.81%) - LeetHub

--- 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
+++ 0746-min-cost-climbing-stairs/0746-min-cost-climbing-stairs.java
@@ -1,9 +1,15 @@
 class Solution {
     public int minCostClimbingStairs(int[] cost) {
         int n = cost.length;
+        if (n == 2) return Math.min(cost[0], cost[1]);
+        
+        int[] dp = new int[n];
+        dp[0] = cost[0];
+        dp[1] = cost[1];
+        
         for (int i = 2; i < n; i++) {
-            cost[i] += Math.min(cost[i - 1], cost[i - 2]);
+            dp[i] = cost[i] + Math.min(dp[i - 1], dp[i - 2]);
         }
-        return Math.min(cost[n - 1], cost[n - 2]);
+       return Math.min(dp[n - 1], dp[n - 2]);
     }
 }
