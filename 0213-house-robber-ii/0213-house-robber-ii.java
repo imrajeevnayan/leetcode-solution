758b120e783008af376386abad427c0df90178fc
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-07-09 16:59:40 +0530

    Time: 0 ms (100%), Space: 42.8 MB (60.06%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,23 +1,25 @@
-class Solution {
-    public int rob(int[] nums) {
-        if (nums == null || nums.length == 0) return 0;
-        if (nums.length == 1) return nums[0];
-        
-        int option1 = robLinear(nums, 0, nums.length - 2);
-        
-        int option2 = robLinear(nums, 1, nums.length - 1);
-        
-        return Math.max(option1, option2);
-    }
-        private int robLinear(int[] nums, int start, int end) {
-        int prev1 = 0; // Max profit up to i-1
-        int prev2 = 0; // Max profit up to i-2
-        
-        for (int i = start; i <= end; i++) {
-            int current = Math.max(prev1, prev2 + nums[i]);
-            prev2 = prev1;
-            prev1 = current;
-        }
-        return prev1;
-    }
+class Solution{
+    private int helper(int[] nums, int start, int end, int[] memo) {
+    if (start > end) return 0;
+    if (memo[start] != -1) return memo[start];
+    int rob = nums[start] + helper(nums, start + 2, end, memo);
+    int skip = helper(nums, start + 1, end, memo);
+  return memo[start] = Math.max(rob, skip);
+   }
+   public int rob(int[] nums) {
+    int n = nums.length;
+    if (n == 1) return nums[0];
+
+    int[] memo1 = new int[n];
+    int[] memo2 = new int[n];
+    Arrays.fill(memo1, -1);
+    Arrays.fill(memo2, -1);
+
+    int robFirst = helper(nums, 0, n - 2, memo1);
+    
+    int robLast = helper(nums, 1, n - 1, memo2);
+    
+    return Math.max(robFirst, robLast);
+}
+
 }
