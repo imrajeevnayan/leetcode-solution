4cacef315ef4675f5a067bfb125c63364f893b44
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-07-13 01:37:09 +0530

    Time: 31 ms (59.78%), Space: 75.4 MB (57.96%) - LeetHub

---
+++ 1331-rank-transform-of-an-array/1331-rank-transform-of-an-array.java
@@ -0,0 +1,23 @@
+class Solution {
+    public int[] arrayRankTransform(int[] arr) {
+        int[] temp = arr.clone();
+        Arrays.sort(temp);
+
+        HashMap<Integer, Integer> map = new HashMap<>();
+        int rank = 1;
+
+        for (int num : temp) {
+            if (!map.containsKey(num)) {
+                map.put(num, rank++);
+            }
+        }
+
+        int[] ans = new int[arr.length];
+
+        for (int i = 0; i < arr.length; i++) {
+            ans[i] = map.get(arr[i]);
+        }
+
+        return ans;
+    }
+}
