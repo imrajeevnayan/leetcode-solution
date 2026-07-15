8f0546780fa5a2eb7d85d4c26efebaac0c39967d
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-07-15 15:08:16 +0530

    Time: 9 ms (21.02%), Space: 46.6 MB (50.16%) - LeetHub

--- 0692-top-k-frequent-words/0692-top-k-frequent-words.java
+++ 0692-top-k-frequent-words/0692-top-k-frequent-words.java
@@ -1,14 +1,13 @@
 class Solution {
     public List<String> topKFrequent(String[] words, int k) {
-    HashMap<String,Integer>map=new HashMap<>();
-    for(String s:words)map.put(s,map.getOrDefault(s,0)+1);
-    PriorityQueue<String>pq=new PriorityQueue<>((a,b)->{
-        if(map.get(a).equals(map.get(b))) {return a.compareTo(b);}
-        return map.get(b)-map.get(a);
-    });
-    pq.addAll(map.keySet());
-    ArrayList<String>ans=new ArrayList<>();  
-    for(int i=0;i<k;i++)ans.add(pq.poll());
-    return ans;
-  }
+        HashMap<String,Integer>map=new HashMap<>();
+        for(String w:words)map.put(w,map.getOrDefault(w,0)+1);
+        PriorityQueue<String> pq = new PriorityQueue<>((a, b) -> 
+            map.get(a).equals(map.get(b)) ? a.compareTo(b) : map.get(b) - map.get(a)
+        );        pq.addAll(map.keySet());
+        List<String>ans=new ArrayList<>();
+        for(int i=0;i<k;i++)ans.add(pq.poll());
+        return ans;
+        
+    }
 }
