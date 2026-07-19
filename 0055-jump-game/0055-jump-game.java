5dad0c43de5e4a663fb1601152cc49b6c0215004
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-07-20 01:56:42 +0530

    Time: 368 ms (5.02%), Space: 47.8 MB (63.13%) - LeetHub

--- 0055-jump-game/0055-jump-game.java
+++ 0055-jump-game/0055-jump-game.java
@@ -1,10 +1,25 @@
 class Solution {
     public boolean canJump(int[] nums) {
-        int max=0;
-        for(int i=0;i<nums.length;i++){
-            if(i > max) return false;
-            max=Math.max(max,i+nums[i]);
+        int[] dp = new int[nums.length];
+        Arrays.fill(dp, -1);
+        return solve(nums, 0,dp);
+    }
+    private boolean solve(int[] nums, int i, int[] dp) {
+        // Base Cases--
+        if (i == nums.length - 1) return true;
+        if (i >= nums.length) return false;
+        if (nums[i] == 0) return false;
+
+        if (dp[i] != -1)return dp[i] == 1;
+
+        boolean reAns = false;
+        for (int jump = 1; jump <= nums[i]; jump++) {
+            if (solve(nums, i + jump, dp)) {
+                reAns = true;
+                break; 
+            }
         }
-        return true;
+        dp[i] = reAns ? 1 : 0;
+        return reAns;
     }
 }
