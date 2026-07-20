b8f52877fa410a5fd2b98d59900015d92941ce03
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-07-20 19:28:09 +0530

    Time: 0 ms (100%), Space: 42.6 MB (90.58%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,25 +1,25 @@
-class Solution{
-    private int helper(int[] nums, int start, int end, int[] memo) {
-    if (start > end) return 0;
-    if (memo[start] != -1) return memo[start];
-    int rob = nums[start] + helper(nums, start + 2, end, memo);
-    int skip = helper(nums, start + 1, end, memo);
-  return memo[start] = Math.max(rob, skip);
-   }
-   public int rob(int[] nums) {
-    int n = nums.length;
-    if (n == 1) return nums[0];
+class Solution {
+    public int rob(int[] nums) {
+        int n = nums.length;
 
-    int[] memo1 = new int[n];
-    int[] memo2 = new int[n];
-    Arrays.fill(memo1, -1);
-    Arrays.fill(memo2, -1);
+        if (n == 1) return nums[0];
 
-    int robFirst = helper(nums, 0, n - 2, memo1);
-    
-    int robLast = helper(nums, 1, n - 1, memo2);
-    
-    return Math.max(robFirst, robLast);
-}
+        return Math.max(
+            robRange(nums, 0, n - 2),
+            robRange(nums, 1, n - 1)
+        );
+    }
 
+    private int robRange(int[] nums, int start, int end) {
+        int prev1 = 0;
+        int prev2 = 0;
+
+        for (int i = start; i <= end; i++) {
+            int curr = Math.max(prev1, prev2 + nums[i]);
+            prev2 = prev1;
+            prev1 = curr;
+        }
+
+        return prev1;
+    }
 }
