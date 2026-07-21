601409be250b287eeb5ab6106814bd476b9d6f3f
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-07-21 18:15:22 +0530

    Time: 0 ms (100%), Space: 43 MB (23.32%) - LeetHub

--- 0213-house-robber-ii/0213-house-robber-ii.java
+++ 0213-house-robber-ii/0213-house-robber-ii.java
@@ -1,25 +1,21 @@
 class Solution {
-    public int rob(int[] nums) {
-        int n = nums.length;
-
-        if (n == 1) return nums[0];
-
-        return Math.max(
-            robRange(nums, 0, n - 2),
-            robRange(nums, 1, n - 1)
-        );
+    int solve(int i, int[] nums, int end, int[] dp) {
+        if (i > end) return 0;
+        if (dp[i] != -1) return dp[i];
+        int take = nums[i] + solve(i + 2, nums, end, dp);
+        int skip = solve(i + 1, nums, end, dp);
+        return dp[i] = Math.max(take, skip);
     }
 
-    private int robRange(int[] nums, int start, int end) {
-        int prev1 = 0;
-        int prev2 = 0;
-
-        for (int i = start; i <= end; i++) {
-            int curr = Math.max(prev1, prev2 + nums[i]);
-            prev2 = prev1;
-            prev1 = curr;
-        }
-
-        return prev1;
+    public int rob(int[] nums) {
+        int n = nums.length;
+        if (n == 1) return nums[0];
+        int[] dp1 = new int[n];
+        int[] dp2 = new int[n];
+        Arrays.fill(dp1, -1);
+        Arrays.fill(dp2, -1);
+        int case1 = solve(0, nums, n - 2, dp1);
+        int case2 = solve(1, nums, n - 1, dp2);
+        return Math.max(case1, case2);
     }
 }
