5f2e17bacc4ca02d149d99b0c4b2a06dac41fdb1
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-07-23 15:39:26 +0530

    Time: 36 ms (99.5%), Space: 51 MB (72.27%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,20 +1,23 @@
 class Solution {
     public int countPrimes(int n) {
-        if (n <= 2) return 0;
-        boolean[] prime = new boolean[n];
-        Arrays.fill(prime, true);
-        prime[0] =prime[1] = false;
-        for (int i = 2; i * i < n; i++) {
-            if (prime[i]) {
-                for (int j = i * i; j < n; j += i) {
-                    prime[j] = false;
+         if (n <= 2) {
+            return 0;
+        }
+
+        boolean[] composite = new boolean[n];
+        int count = n / 2; // Assume all odd numbers are prime
+
+        for (int i = 3; i * i < n; i += 2) {
+            if (!composite[i]) {
+                for (int j = i * i; j < n; j += 2 * i) {
+                    if (!composite[j]) {
+                        composite[j] = true;
+                        count--;
+                    }
                 }
             }
         }
-        int count = 0;
-        for (int i = 2; i < n; i++) {
-            if (prime[i]) count++;
-        }
+
         return count;
     }
 }
