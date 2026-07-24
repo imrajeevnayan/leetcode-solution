92b1880dceebd0ae70bf7c7207151a9afbeee02d
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-07-24 16:49:56 +0530

    Time: 8 ms (16.95%), Space: 47.9 MB (12.19%) - LeetHub

--- 0268-missing-number/0268-missing-number.java
+++ 0268-missing-number/0268-missing-number.java
@@ -1,9 +1,9 @@
 class Solution {
     public int missingNumber(int[] nums) {
-      int n=nums.length;
-        int total=n*(n+1)/2;
-        int sum=0;
-        for(int num:nums)sum+=num;
-        return total-sum;
+        Arrays.sort(nums);
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=i) return i;
+        }
+        return nums.length;
     }
 }
