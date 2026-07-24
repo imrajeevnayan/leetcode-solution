32e9625390c9b03053aae44d8da6559043d2a320
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-07-24 22:45:59 +0530

    Time: 84 ms (93.28%), Space: 51.3 MB (22.93%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,6 +1,6 @@
 class Solution {
     public int countPrimes(int n) {
-        if(n <2)return 0;
+        if(n <=2)return 0;
         boolean []prime=new boolean[n];
         Arrays.fill(prime,true);
         prime[0]=false;
