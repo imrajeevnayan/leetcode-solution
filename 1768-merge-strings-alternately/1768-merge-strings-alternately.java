def5f484bb13116b178ca10f9febfa01ef93e172
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-07-27 16:03:55 +0530

    Time: 1 ms (96.15%), Space: 43.2 MB (48.77%) - LeetHub

--- 1768-merge-strings-alternately/1768-merge-strings-alternately.java
+++ 1768-merge-strings-alternately/1768-merge-strings-alternately.java
@@ -3,10 +3,8 @@ class Solution {
         StringBuilder sb = new StringBuilder();
         int i = 0, j = 0;
         while (i < word1.length() || j < word2.length()) {
-            if (i < word1.length())
-                sb.append(word1.charAt(i++));
-            if (j < word2.length())
-                sb.append(word2.charAt(j++));
+            if (i < word1.length())sb.append(word1.charAt(i++));
+            if (j < word2.length()) sb.append(word2.charAt(j++));
         }
         return sb.toString();
     }
