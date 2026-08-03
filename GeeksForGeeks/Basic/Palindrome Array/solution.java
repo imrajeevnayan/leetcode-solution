e15e289a3c7dbf447e6abf0cc0c650816b378582
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-08-03 22:16:05 +0530

    [AlgoCommit] GeeksForGeeks/Basic/Palindrome Array

---
+++ GeeksForGeeks/Basic/Palindrome Array/solution.java
@@ -0,0 +1,11 @@
+class Solution {
+    public static boolean isPalindrome(int[] arr) {
+        int left = 0,right = arr.length - 1;
+        while (left < right) {
+            if (arr[left] != arr[right])return false;
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
