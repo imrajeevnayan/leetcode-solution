8ac90ebc66f9dd5449665660820a50bb46833b68
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-08-07 02:05:52 +0530

    Time: 1 ms (98.09%), Space: 43.8 MB (6.84%) - LeetHub

--- 3340-check-balanced-string/3340-check-balanced-string.java
+++ 3340-check-balanced-string/3340-check-balanced-string.java
@@ -1,11 +1,17 @@
 class Solution {
     public boolean isBalanced(String num) {
-        int evenSum = 0,oddSum = 0;
+        int balance = 0;
+
         for (int i = 0; i < num.length(); i++) {
             int digit = num.charAt(i) - '0';
-            if (i % 2 == 0)  evenSum += digit;
-            else   oddSum += digit;
+
+            if (i % 2 == 0) {
+                balance += digit;
+            } else {
+                balance -= digit;
+            }
         }
-        return evenSum == oddSum;
+
+        return balance == 0;
     }
 }
