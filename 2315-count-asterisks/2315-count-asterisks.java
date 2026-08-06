5b707556171e7ad79627f2a2395dbc18a84230c5
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-08-07 01:56:37 +0530

    Time: 1 ms (99.06%), Space: 42.9 MB (73.8%) - LeetHub

---
+++ 2315-count-asterisks/2315-count-asterisks.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int countAsterisks(String s) {
+        int count = 0;
+        boolean insideBars = false;
+
+        for (char ch : s.toCharArray()) {
+            if (ch == '|') {
+                insideBars = !insideBars;
+            } else if (ch == '*' && !insideBars) {
+                count++;
+            }
+        }
+
+        return count;
+    }
+}
