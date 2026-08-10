acaeecf0b248259406dd73758c416a9d89f2fd65
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-08-11 00:36:58 +0530

    Time: 8 ms (17.83%), Space: 48.2 MB (53.68%) - LeetHub

---
+++ 1302-deepest-leaves-sum/1302-deepest-leaves-sum.java
@@ -0,0 +1,29 @@
+class Solution {
+    public int deepestLeavesSum(TreeNode root) {
+        Queue<TreeNode> queue = new LinkedList<>();
+        queue.offer(root);
+
+        int sum = 0;
+
+        while (!queue.isEmpty()) {
+            int size = queue.size();
+            sum = 0; // reset for each level
+
+            for (int i = 0; i < size; i++) {
+                TreeNode node = queue.poll();
+
+                sum += node.val;
+
+                if (node.left != null) {
+                    queue.offer(node.left);
+                }
+
+                if (node.right != null) {
+                    queue.offer(node.right);
+                }
+            }
+        }
+
+        return sum;
+    }
+}
