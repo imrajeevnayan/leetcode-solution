dc6b6f22180c9b9e0f40cc45e37aac42617db5d6
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-08-22 20:26:39 +0530

    Time: 0 ms (100%), Space: 42.9 MB (26.04%) - LeetHub

--- 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
+++ 1863-sum-of-all-subset-xor-totals/1863-sum-of-all-subset-xor-totals.java
@@ -3,9 +3,8 @@ class Solution {
         return dfs(nums, 0, 0);
     }
     private int dfs(int[] nums, int index, int xor) {
-        if (index == nums.length) {
-            return xor;
-        }
+        if (index == nums.length) return xor;
+        
         int exclude = dfs(nums, index + 1, xor);
         int include = dfs(nums, index + 1, xor ^ nums[index]);
         return exclude + include;
