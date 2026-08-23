fd100c51468234aaef55dfefe0c387b696486523
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-08-23 21:38:21 +0530

    Time: 549 ms (85.42%), Space: 0B (100%) - LeetHub

--- 0183-customers-who-never-order/0183-customers-who-never-order.sql
+++ 0183-customers-who-never-order/0183-customers-who-never-order.sql
@@ -1,4 +1,4 @@
-select c.name as Customers
-from Customers c left join Orders o
-on  c.id=o.customerId
-where o.customerId is null;
+SELECT c.name AS Customers
+FROM Customers c
+LEFT JOIN Orders o ON c.id = o.customerId
+WHERE o.id IS NULL;
