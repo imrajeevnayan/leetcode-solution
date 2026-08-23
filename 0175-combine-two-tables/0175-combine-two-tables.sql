e18b1dec54617cda4a63e0b97da422d1afe6ca7d
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-08-23 21:28:32 +0530

    Time: 522 ms (21.04%), Space: 0B (100%) - LeetHub

--- 0175-combine-two-tables/0175-combine-two-tables.sql
+++ 0175-combine-two-tables/0175-combine-two-tables.sql
@@ -1,3 +1,4 @@
-SELECT firstName, lastName, city, state
-FROM  Person
-LEFT JOIN Address USING (personId);
+SELECT p.firstName,p.lastName,a.city,a.state
+FROM Person p
+LEFT JOIN Address a
+ON p.personId = a.personId;
