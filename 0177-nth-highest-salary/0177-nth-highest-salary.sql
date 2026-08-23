c97b5ef9dfa33ad3f1573516816611b2ce225021
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-08-23 21:34:45 +0530

    Time: 447 ms (79.6%), Space: 0B (100%) - LeetHub

--- 0177-nth-highest-salary/0177-nth-highest-salary.sql
+++ 0177-nth-highest-salary/0177-nth-highest-salary.sql
@@ -1,10 +1,11 @@
 CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
 BEGIN
-    SET N = N - 1;
     RETURN (
-        SELECT DISTINCT salary
-        FROM Employee
-        ORDER BY salary DESC
-        LIMIT 1 OFFSET N
+        SELECT salary FROM (
+            SELECT salary, DENSE_RANK() OVER(ORDER BY salary DESC) AS rnk
+            FROM Employee
+        ) ranked
+        WHERE rnk = N
+        LIMIT 1
     );
 END
