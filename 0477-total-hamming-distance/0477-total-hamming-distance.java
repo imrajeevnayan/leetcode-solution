b418bcffeff269a51a43c35c2aeae40ff211d3b0
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-09-01 01:43:24 +0530

    Time: 15 ms (67.93%), Space: 47.8 MB (87.34%) - LeetHub

---
+++ 0477-total-hamming-distance/0477-total-hamming-distance.java
@@ -0,0 +1,16 @@
+class Solution {
+    public int totalHammingDistance(int[] nums) {
+        int ans = 0;
+        for (int bit = 0; bit < 32; bit++) {
+            int ones = 0;
+            for (int num : nums) {
+                if ((num & (1 << bit)) != 0) {
+                    ones++;
+                }
+            }
+            int zeros = nums.length - ones;
+            ans += ones * zeros;
+        }
+        return ans;
+    }
+}
