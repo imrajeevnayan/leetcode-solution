5db7be8b18f9c0ce5dc2d08548db3a2af4cea428
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-09-03 11:06:26 +0530

    Time: 0 ms (100%), Space: 43.9 MB (47.18%) - LeetHub

--- 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
+++ 0033-search-in-rotated-sorted-array/0033-search-in-rotated-sorted-array.java
@@ -13,7 +13,7 @@ class Solution {
             else{
                 if (nums[mid] < target && target <= nums[high]) low = mid + 1;
                  else  high = mid - 1;
-                
+                 
             }
         }
         return ans;
