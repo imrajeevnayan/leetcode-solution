dc1179068d30dcbf51bee6a16480ee9c920a971a
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-09-04 23:42:53 +0530

    [AlgoCommit] GeeksForGeeks/Basic/Insert at Middle of Linked List

---
+++ GeeksForGeeks/Basic/Insert at Middle of Linked List/solution.java
@@ -0,0 +1,17 @@
+class Solution {
+    public Node insertInMiddle(Node head, int x) {
+        Node newNode = new Node(x);
+        if (head == null)  return newNode;
+    
+        Node slow = head,fast = head;
+
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        newNode.next = slow.next;
+        slow.next = newNode;
+
+        return head;
+    }
+}
