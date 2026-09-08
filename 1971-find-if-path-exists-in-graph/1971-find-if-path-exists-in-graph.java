f0b94245fcfaa27a43ae3f7380c39d86b9786657
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-09-08 17:40:41 +0530

    Time: 80 ms (67.11%), Space: 286 MB (36%) - LeetHub

--- 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
+++ 1971-find-if-path-exists-in-graph/1971-find-if-path-exists-in-graph.java
@@ -1,39 +1,28 @@
 class Solution {
-    public boolean validPath( int n,int[][] edges,int source, int destination) {
-        // Create adjacency list
-        List<List<Integer>> graph = new ArrayList<>();
-
-        for (int i = 0; i < n; i++) {
-            graph.add(new ArrayList<>());
-        }
-        // Build undirected graph
-        for (int[] edge : edges) {
-            int u = edge[0];
-            int v = edge[1];
-
+    public boolean validPath(int n, int[][] edges, int source, int destination) {
+        List<List<Integer>>graph=new ArrayList<>();
+        for(int i=0;i<n;i++)graph.add(new ArrayList<>());
+        for(int []edge:edges){
+            int u=edge[0];
+            int v=edge[1];
             graph.get(u).add(v);
             graph.get(v).add(u);
         }
-        // Track visited nodes
-        boolean[] visited = new boolean[n];
-
-        // Start DFS
-        return dfs(source, destination, graph, visited );
-    }
-    private boolean dfs( int node,int destination,List<List<Integer>> graph, boolean[] visited) {
-        // Destination found
-        if (node == destination)return true;
-        // Mark visited
-        visited[node] = true;
-
-        // Visit neighbours
-        for (int next : graph.get(node)) {
-
-            if (!visited[next]) {
-                if (dfs( next, destination, graph, visited)) return true;  
+        boolean visited[]=new boolean[n];
+        Stack<Integer>st=new Stack<>();
+        st.add(source);
+        visited[source]=true;
+        while(!st.isEmpty()){
+            int node=st.pop();
+            if(node==destination) return true;
+            for(int next:graph.get(node)){
+                if(!visited[next]){
+                    visited[next]=true;
+                    st.add(next);
+                }
             }
         }
-
         return false;
+        
     }
 }
