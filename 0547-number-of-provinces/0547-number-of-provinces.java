37b2b2ab0c6bb446d6568f89140272d07f9b096f
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-09-09 12:16:49 +0530

    Time: 2 ms (41.61%), Space: 47 MB (87.8%) - LeetHub

--- 0547-number-of-provinces/0547-number-of-provinces.java
+++ 0547-number-of-provinces/0547-number-of-provinces.java
@@ -3,23 +3,21 @@ class Solution {
         int n = isConnected.length;
         boolean[] visited = new boolean[n];
         int provinces = 0;
-
         for (int i = 0; i < n; i++) {
-
-            // New unvisited node = new province
+            // New connected component found
             if (!visited[i]) {
                 provinces++;
-                Queue<Integer> queue = new ArrayDeque<>();
-                queue.offer(i);
+                Deque<Integer> stack = new ArrayDeque<>();
+                stack.push(i);
                 visited[i] = true;
-                while (!queue.isEmpty()) {
-                    int curr = queue.poll();
+                while (!stack.isEmpty()) {
+                    int curr= stack.pop();
                     // Check all possible neighbors
                     for (int j = 0; j < n; j++) {
 
-                        if (isConnected[curr][j] == 1 && !visited[j]) {
+                        if (isConnected[curr][j] == 1  && !visited[j]) {
                             visited[j] = true;
-                            queue.offer(j);
+                            stack.push(j);
                         }
                     }
                 }
