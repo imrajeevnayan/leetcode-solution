419d84ad63c101a29c3b4f9b2108b5d4c218958e
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-09-16 12:38:44 +0530

    Time: 545 ms (44.97%), Space: 80.1 MB (7.54%) - LeetHub

--- 0204-count-primes/0204-count-primes.java
+++ 0204-count-primes/0204-count-primes.java
@@ -1,20 +1,32 @@
 class Solution {
     public int countPrimes(int n) {
-        if(n <2) return 0;
-        boolean []isPrime=new boolean[n];
-        Arrays.fill(isPrime,true);
-        isPrime[0]=isPrime[1]=false;
-        for(int i=2;i*i<n;i++){
-            if(isPrime[i]){
-                for(int j=i*i;j<n;j+=i){
-                    isPrime[j]=false;
+        if (n <= 2) return 0;
+        boolean[] isPrime = new boolean[n];
+        Arrays.fill(isPrime, true);
+        isPrime[0] = isPrime[1] = false;
+        // 2 ke multiples ko false karo
+        for (int i = 4; i < n; i += 2) {
+            isPrime[i] = false;
+        }
+        // Sirf odd numbers check karo
+        for (int i = 3; i * i < n; i += 2) {
+            if (isPrime[i]) {
+
+                for (int j = i * i; j < n; j += 2 * i) {
+                    isPrime[j] = false;
                 }
             }
         }
-        int count=0;
-        for(int i=2;i<n;i++){
-            if(isPrime[i])count++;
+
+        int count = 1; // 2 prime hai
+
+        // Sirf odd numbers count karo
+        for (int i = 3; i < n; i += 2) {
+            if (isPrime[i]) {
+                count++;
+            }
         }
+
         return count;
     }
 }
