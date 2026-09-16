26d809af046ed6f68bcff6fe189c35dbf8492663
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-09-16 17:42:18 +0530

    Time: 0 ms (100%), Space: 42.3 MB (17.47%) - LeetHub

--- 0070-climbing-stairs/0070-climbing-stairs.java
+++ 0070-climbing-stairs/0070-climbing-stairs.java
@@ -1,13 +1,15 @@
 class Solution {
-    static int solve(int n ,int[]dp){
-        if(n==0)return 1;
-        if(n <0)return 0;
-        if(dp[n]!=-1)return dp[n];
-        return dp[n]= solve(n-1,dp)+solve(n-2,dp);
+    int[] dp;
+    private int solve(int n) {
+        if (n <= 1) return 1;
+        if (dp[n] != -1) return dp[n];
+        dp[n] = solve(n - 1) + solve(n - 2);
+        return dp[n];
     }
+
     public int climbStairs(int n) {
-        int dp[]=new int[n+1];
-        Arrays.fill(dp,-1);
-        return solve(n,dp );
+        dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return solve(n);
     }
 }
