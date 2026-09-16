a7cacc10bf0423ca33be1ca295a382c51a6aeea6
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-09-16 16:42:27 +0530

    Time: 8 ms (57.76%), Space: 47 MB (62.25%) - LeetHub

--- 1603-design-parking-system/1603-design-parking-system.java
+++ 1603-design-parking-system/1603-design-parking-system.java
@@ -1,13 +1,22 @@
 class ParkingSystem {
-    int[] parking;
-
+    // STATE: bache hue slots (instance variables)
+    private int big, medium, small;
+    
+    // CONSTRUCTOR: shuruati state set
     public ParkingSystem(int big, int medium, int small) {
-        parking = new int[] {0, big, medium, small};
+        this.big = big;       // this.big = instance var, big = parameter
+        this.medium = medium;
+        this.small = small;
     }
+    
+    // BEHAVIOR
     public boolean addCar(int carType) {
-        if (parking[carType] > 0) {
-            parking[carType]--;
-            return true;
+        if (carType == 1) {
+            if (big > 0) { big--; return true; }
+        } else if (carType == 2) {
+            if (medium > 0) { medium--; return true; }
+        } else { // carType == 3
+            if (small > 0) { small--; return true; }
         }
         return false;
     }
