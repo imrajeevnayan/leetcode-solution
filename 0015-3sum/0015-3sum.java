794bc910af32d84254115696f160fcc743399620
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-09-18 17:56:00 +0530

    Time: 905 ms (6.34%), Space: 60.2 MB (11.35%) - LeetHub

--- 0015-3sum/0015-3sum.java
+++ 0015-3sum/0015-3sum.java
@@ -1,32 +1,23 @@
 class Solution {
     public List<List<Integer>> threeSum(int[] nums) {
-        List<List<Integer>> result = new ArrayList<>();
-        Arrays.sort(nums);
-
+        Arrays.sort(nums);                       // two pointer ke liye zaroori
+        Set<List<Integer>> set = new HashSet<>(); // DUPLICATES KA KHAYAL SET RAKHEGA!
+        
         for (int i = 0; i < nums.length - 2; i++) {
-            if (i > 0 && nums[i] == nums[i - 1]) continue;
-            if (nums[i] > 0) break;
-
-            int left = i + 1;
-            int right = nums.length - 1;
-
+            int left = i + 1, right = nums.length - 1;
+            
             while (left < right) {
                 int sum = nums[i] + nums[left] + nums[right];
-
-                if (sum == 0) {
-                    result.add(Arrays.asList(nums[i], nums[left], nums[right]));
-
-                    while (left < right && nums[left] == nums[left + 1]) left++;
-                    while (left < right && nums[right] == nums[right - 1]) right--;
-
-                    left++;
-                    right--;
-                } 
-                else if (sum < 0)  left++;
-                else right--;
                 
+                if (sum == 0) {
+                    set.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    left++;      // bas aage badho — 
+                    right--;     // duplicate ho toh bhi SET sambhal lega!
+                }
+                else if (sum < 0)  left++;
+                else               right--;
             }
         }
-        return result;
+        return new ArrayList<>(set);  // Set → List (final answer format)
     }
 }
