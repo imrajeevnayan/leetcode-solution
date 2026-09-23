8baeef8ab4c880e99eb44e7d19808704695df206
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-09-23 15:03:35 +0530

    Time: 3 ms (86.14%), Space: 43.1 MB (85.35%) - LeetHub

--- 0020-valid-parentheses/0020-valid-parentheses.java
+++ 0020-valid-parentheses/0020-valid-parentheses.java
@@ -1,20 +1,25 @@
 class Solution {
     public boolean isValid(String s) {
+        // Agar length odd hai, toh valid hona impossible hai.
+        // Kyunki har bracket ka pair hona zaroori hai.
+        if (s.length() % 2 != 0) return false;
         Stack<Character> stack = new Stack<>();
 
         for (char c : s.toCharArray()) {
-            if (c == '(' || c == '{' || c == '[') {
-                stack.push(c);
-            } else {
+            // Opening brackets ko stack mein push karo
+            if (c == '(' || c == '{' || c == '[') stack.push(c);
+            else {
+                // Closing bracket mila
+                // Agar stack khali hai, toh koi matching opening bracket nahi hai
                 if (stack.isEmpty()) return false;
-
                 char top = stack.pop();
+                // Check karo ki pair match kar raha hai ya nahi
                 if (c == ')' && top != '(') return false;
                 if (c == '}' && top != '{') return false;
                 if (c == ']' && top != '[') return false;
             }
         }
-
+        // End mein stack khali hona chahiye tabhi string valid hai
         return stack.isEmpty();
     }
 }
