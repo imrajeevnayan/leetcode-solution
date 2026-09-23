5c6ff800682738aebb071eaa29cfb5c86cc1ed3e
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-09-23 16:06:29 +0530

    Time: 0 ms (100%), Space: 42.2 MB (33.74%) - LeetHub

--- 0509-fibonacci-number/0509-fibonacci-number.java
+++ 0509-fibonacci-number/0509-fibonacci-number.java
@@ -1,12 +1,18 @@
 class Solution {
-    public int fib(int n) {
-        if(n <1)return 0;
-        int dp[]=new int[n+1];
-        dp[0]=0;
-        dp[1]=1;
-        for(int i=2;i<=n;i++){
-            dp[i]=dp[i-1]+dp[i-2];
-        }
+    private int helper(int n, int[] dp) {
+        // Base Case
+        if (n <= 1) return n;
+        
+        // Step 1: Check if already calculated
+        if (dp[n] != -1)  return dp[n];
+    
+        // Step 2: Calculate and Store
+        dp[n] = helper(n - 1, dp) + helper(n - 2, dp);
         return dp[n];
+    }
+    public int fib(int n) {
+        int[] dp = new int[n + 1];
+        Arrays.fill(dp, -1);
+        return helper(n, dp);
     }
 }
