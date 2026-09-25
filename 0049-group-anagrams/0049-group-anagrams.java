251c752954d249f28e22601469895c2cd6c4e831
RAJEEV NAYAN <immrrobot121@gmail.com>
2026-09-25 18:31:59 +0530

    Time: 7 ms (72.25%), Space: 49.6 MB (73.07%) - LeetHub

--- 0049-group-anagrams/0049-group-anagrams.java
+++ 0049-group-anagrams/0049-group-anagrams.java
@@ -1,12 +1,13 @@
 class Solution {
     public List<List<String>> groupAnagrams(String[] strs) {
-      HashMap<String,List<String>>ans=new HashMap<>();
-        for(String s:strs){
-            char c[]=s.toCharArray();
-            Arrays.sort(c);
-            String key=String.valueOf(c);
-            ans.computeIfAbsent(key,k->new ArrayList<>()).add(s);
-        }
-        return new ArrayList<>(ans.values());
+     HashMap<String,List<String>>ans=new HashMap<>();
+     for(String s:strs){
+        char ch[]=s.toCharArray();
+        Arrays.sort(ch);
+        String key=String.valueOf(ch);
+        ans.computeIfAbsent(key,k->new ArrayList<>()).add(s);
+     }
+     return new ArrayList<>(ans.values());
+   }
+
     }
-}
